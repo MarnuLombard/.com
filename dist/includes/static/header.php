ef62f8557d379bd7f0aaff63003699e6975fb9e9
MarnuLombard <marnu@lombard>
2014-01-26 07:58:47 +0200

    major refactor on header nav bar made the scss far more readable and maintainable made breakpoints mobile first

--- dist/includes/static/header.php
+++ dist/includes/static/header.php
@@ -1,20 +1,20 @@
 <header class="header--main">
   <nav class="nav--header grid" role="navigation">
 
-    <a href="/" class="grid__item one-twelfth logo__wrapper">
+    <a href="/" class="grid__item logo__wrapper">
       <img src="img/logo.svg" alt="" class="logo">
     </a>
 
     <div class="nav__link__wrapper grid__item">
-      <?php echo navLinks($navArray, true); ?>
+      <?php echo navLinks($navArray, false); ?>
     </div>
 
-    <div class="grid__item one-twelfth icon__wrapper">
-      <a href="https://github.com/marnulombard/" class="icon icon-github nav__link--transition" target="_blank">
-        <span>Github</span>
+    <div class="grid__item icon__wrapper">
+      <a href="https://github.com/marnulombard/" class="icon icon-github" target="_blank">
+        <span class="nav__text">Github</span>
       </a>
-      <a href="http://twitter.com/mr_springtime" class="icon icon-twitter nav__link--transition" target="_blank">
-        <span>Twitter</span>
+      <a href="http://twitter.com/mr_springtime" class="icon icon-twitter" target="_blank">
+        <span class="nav__text">Twitter</span>
       </a>
     </div>
   </nav>
