eca49ad13f8c83ac8d9549b64f7b3bcbe03aeda0
Marnu Lombard <marnu@lombard>
2014-08-22 07:45:00 +0200

    Add Mayat Nurick Langa

---
+++ dist/posts/mayat.php
@@ -0,0 +1,16 @@
+<?php
+
+$postData = array(
+
+'tags' => 'Front_End_Development Back_End_Development 2014 Laravel',
+'markdown' => "
+###Mayat, Nurick, Langa Inc
+
+A simple one-page site for a law firm in Johannesburg
+I was not responsible for the design of this.
+
+**Notable features:**
+* Usage of business-related [Schema.org](http://schema.org/) markup to improve SEO
+* A simple CMS built on top of Laravel -- which I recognise is a bit crazy for a one page site.
+[Live site](http://mayatnuricklangainc.co.za)",
+);
