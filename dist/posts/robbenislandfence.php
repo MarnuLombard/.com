1020fb168c17ae3d7bdead11c152082a4cb4fcc9
Marnu Lombard <marnu@lombard>
2014-08-22 07:39:10 +0200

    Add robbenislandfence

---
+++ dist/posts/robbenislandfence.php
@@ -0,0 +1,19 @@
+<?php
+
+$postData = array(
+
+'tags' => 'Front_End_Development Back_End_Development 2014 Laravel Under_Development',
+'markdown' => "
+###Robben Island Fence
+
+Still under under construction.
+An online shop selling artworks.
+I was not responsible for the design of this.
+A content management system to upload and manage the artworks.
+
+**Notable features:**
+* Plugs into DHL's API to fetch prices and place orders
+* Various permutations of artworks, frames and sizes - each unquely priced. Made for an interesting architectural problem to solve.
+* A simple CMS built on top of Laravel
+[Live site](http://104.131.244.207)",
+);
